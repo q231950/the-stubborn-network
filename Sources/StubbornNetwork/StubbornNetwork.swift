ff2f5d12e33405ae2770fca4c5a5d3f0c42ac1fa
Martin Kim Dund-Pham <martinkim.pham@gmail.com>
2019-10-01 14:17:12 +0200

    Cleanup file that defines the API

--- Sources/StubbornNetwork/StubbornNetwork.swift
+++ Sources/StubbornNetwork/StubbornNetwork.swift
@@ -10,13 +10,22 @@ import Foundation
 /// Stubbing your network can greatly improve flakiness in UI tests and is a common practice
 /// for unit tests. You can also use stubbed network responses for running SwiftUI Previews
 /// more efficiently where the stubs act like a cache.
-public struct StubbornNetwork {
+public struct StubbornNetwork {}
 
-    /// Shorthand for a `StubbornURLSession` with a `.ephemeral` configutation. Creates a stubbed `URLSession` that lives in memory only. The stubs of this `StubbornURLSession` are not persisted anywhere. This factory method is useful in unit tests.
+// MARK: Ephemeral stubbed URLSessions
+
+extension StubbornNetwork {
+
+    /// Creates a stubbed `URLSession` that lives in memory only. The stubs of this `StubbornURLSession` are not persisted anywhere. This factory method is useful in unit tests.
     /// - Parameter stubbornURLSession: The mutable `StubbornURLSession`. Use the closure's parameter to modify the record mode of the `StubbornURLSession` or to stub requests.
     public static func ephemeralStubbedURLSession(_ stubbornURLSession:((StubbornURLSession) -> Void)? = nil) -> URLSession {
         return stubbed(withConfiguration: .ephemeral, stubbornURLSession)
     }
+}
+
+// MARK: Persisted stubbed URLSessions
+
+extension StubbornNetwork {
 
     /// Create a stubbed `URLSession` by providing a `ProcessInfo` that contains information about the location of the source for the stubs
     /// - Parameter processInfo: The process info that contains `EnvironmentVariableKeys` specifying the location of the stub source.
@@ -28,13 +37,21 @@ public struct StubbornNetwork {
         return stubbed(withConfiguration: .persistent(location: location), stubbornURLSession)
     }
 
-    static func persistedStubbedURLSession(withName name: String, path: String, _ stubbornURLSession:((StubbornURLSession) -> Void)? = nil) -> URLSession {
+    /// Create a stubbed `URLSession` by providing a name and a path to the source for the stubs to use.
+    /// - Parameter name: The file name of the `StubSource`
+    /// - Parameter path: The path to the `StubSource`
+    /// - Parameter stubbornURLSession: The mutable `StubbornURLSession`. Use the closure's parameter to modify the record mode of the `StubbornURLSession` or to stub requests.
+    public static func persistedStubbedURLSession(withName name: String, path: String, _ stubbornURLSession:((StubbornURLSession) -> Void)? = nil) -> URLSession {
 
         /// TODO: Move this implementation to an internal static func on `URLSessionStub`
         let location = StubSourceLocation(name: name, path: path)
         return stubbed(withConfiguration: .persistent(location: location), stubbornURLSession)
     }
+}
 
+// MARK: Module Internal Implementation Details
+
+extension StubbornNetwork {
     ///    Creates a stubbed `URLSession` with a configuration.
     /// - Parameter stubbornURLSession: The mutable `StubbornURLSession`. Use the closure's parameter to modify the record mode of the `StubbornURLSession` or to stub requests.
     /// - Parameter configuration
