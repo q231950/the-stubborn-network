16ef26e72b8e30a40bdcbfea44198ea81b33b730
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2019-10-17 16:44:53 +0200

    It's not a Swift version issue but a SwiftPM issue https://bugs.swift.org/browse/SR-11501

--- Tests/StubbornNetworkTests/URLRequestMatcherTests.swift
+++ Tests/StubbornNetworkTests/URLRequestMatcherTests.swift
@@ -9,45 +9,45 @@ import XCTest
 @testable import StubbornNetwork
 
 class URLRequestMatcherTests: XCTestCase {
-    func testMatchesRequestHeadersCaseSensitively() throws {
-        var requestWithCapitalHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+    func testMatchesRequestHeadersCaseSensitively() {
+        var requestWithCapitalHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithCapitalHeader.setValue("alphabet", forHTTPHeaderField: "ABC")
 
-        var requestWithLowercaseHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        var requestWithLowercaseHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithLowercaseHeader.setValue("alphabet", forHTTPHeaderField: "abc")
 
         XCTAssertTrue(requestWithCapitalHeader.matches(request: requestWithLowercaseHeader))
     }
 
-    func testHeaderDuplicateMismatches() throws {
-        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+    func testHeaderDuplicateMismatches() {
+        var requestWithDuplicatedHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithDuplicatedHeader.setValue("alphabet", forHTTPHeaderField: "ABC")
         requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "ABC")
 
-        var requestWithSingleHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        var requestWithSingleHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithSingleHeader.setValue("alphabet", forHTTPHeaderField: "abc")
 
         XCTAssertFalse(requestWithDuplicatedHeader.matches(request: requestWithSingleHeader))
     }
 
-    func testHeaderDuplicateMatches() throws {
-        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+    func testHeaderDuplicateMatches() {
+        var requestWithDuplicatedHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithDuplicatedHeader.addValue("alphabet", forHTTPHeaderField: "ABC")
         requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "ABC")
 
-        var requestWithDuplicatedLowercaseHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        var requestWithDuplicatedLowercaseHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithDuplicatedLowercaseHeader.addValue("alphabet", forHTTPHeaderField: "abc")
         requestWithDuplicatedLowercaseHeader.addValue("алфавит", forHTTPHeaderField: "abc")
 
         XCTAssertTrue(requestWithDuplicatedHeader.matches(request: requestWithDuplicatedLowercaseHeader))
     }
 
-    func testHeaderOrderMismatches() throws {
-        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+    func testHeaderOrderMismatches() {
+        var requestWithDuplicatedHeader = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithDuplicatedHeader.addValue("alphabet", forHTTPHeaderField: "abc")
         requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "abc")
 
-        var requestWithDuplicatedHeaderOtherOrder = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        var requestWithDuplicatedHeaderOtherOrder = URLRequest(url: URL(string: "127.0.0.1")!)
         requestWithDuplicatedHeaderOtherOrder.addValue("алфавит", forHTTPHeaderField: "abc")
         requestWithDuplicatedHeaderOtherOrder.addValue("alphabet", forHTTPHeaderField: "abc")
 
