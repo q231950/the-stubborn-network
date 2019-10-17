c6fc8f2138a569beccda6b51907779141dd076e4
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2019-10-17 16:17:46 +0200

    Add tests for duplicated headers

--- Tests/StubbornNetworkTests/URLRequestMatcherTests.swift
+++ Tests/StubbornNetworkTests/URLRequestMatcherTests.swift
@@ -18,4 +18,39 @@ class URLRequestMatcherTests: XCTestCase {
 
         XCTAssertTrue(requestWithCapitalHeader.matches(request: requestWithLowercaseHeader))
     }
+
+    func testHeaderDuplicateMismatches() throws {
+        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithDuplicatedHeader.setValue("alphabet", forHTTPHeaderField: "ABC")
+        requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "ABC")
+
+        var requestWithSingleHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithSingleHeader.setValue("alphabet", forHTTPHeaderField: "abc")
+
+        XCTAssertFalse(requestWithDuplicatedHeader.matches(request: requestWithSingleHeader))
+    }
+
+    func testHeaderDuplicateMatches() throws {
+        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithDuplicatedHeader.addValue("alphabet", forHTTPHeaderField: "ABC")
+        requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "ABC")
+
+        var requestWithDuplicatedLowercaseHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithDuplicatedLowercaseHeader.addValue("alphabet", forHTTPHeaderField: "abc")
+        requestWithDuplicatedLowercaseHeader.addValue("алфавит", forHTTPHeaderField: "abc")
+
+        XCTAssertTrue(requestWithDuplicatedHeader.matches(request: requestWithDuplicatedLowercaseHeader))
+    }
+
+    func testHeaderOrderMismatches() throws {
+        var requestWithDuplicatedHeader = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithDuplicatedHeader.addValue("alphabet", forHTTPHeaderField: "abc")
+        requestWithDuplicatedHeader.addValue("алфавит", forHTTPHeaderField: "abc")
+
+        var requestWithDuplicatedHeaderDifferentOrder = URLRequest(url: try XCTUnwrap(URL(string: "127.0.0.1")))
+        requestWithDuplicatedHeaderDifferentOrder.addValue("алфавит", forHTTPHeaderField: "abc")
+        requestWithDuplicatedHeaderDifferentOrder.addValue("alphabet", forHTTPHeaderField: "abc")
+
+        XCTAssertFalse(requestWithDuplicatedHeader.matches(request: requestWithDuplicatedHeaderDifferentOrder))
+    }
 }
